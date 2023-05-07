5657b08ab9a5597ebc9285ade96408cdc86ed97d
Sanjana Chalavadi <30472680+Sanjana-Chalavadi@users.noreply.github.com>
2023-05-07 15:44:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My New Healthy Diner')
+
+streamlit.header('Breakfast Menu for Monday')
+streamlit.text('Very Berry Oatmeal')
+streamlit.text('Avocado, Spinach & Banana Smoothie')
+streamlit.text('Scrambled Eggs')              
